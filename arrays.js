64a68bffb182ff7934ae55f643d9fc3fd769e051
zuriel stanley <zuriel.stanley6@gmail.com>
2017-09-06 19:44:42 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -34,6 +34,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
 }
 
 function removeElementFromBeginningOfArray(array) {
-  array.slice()
-  return array
+
+  return array.slice(1)
 }
